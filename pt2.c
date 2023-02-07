5a0d797c6af8a511ef3151c07e944ae8056ac285
lele4202 <124668857+lele4202@users.noreply.github.com>
2023-02-07 12:05:30 +0100

    Add files via upload

---
+++ pt2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+int x;
+int y;
+
+printf("inserisci due numeri: \n");
+scanf("%d %d", &x, &y);
+int moltiplicazione = x*y;
+printf("la moltiplicazione di x e y è: %d\n", moltiplicazione);
+}
