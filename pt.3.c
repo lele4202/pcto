790bd439a8ddd213951342a7c771d50717e79938
lele4202 <124668857+lele4202@users.noreply.github.com>
2023-02-07 12:51:07 +0100

    Add files via upload

---
+++ pt.3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+
+    printf("inserisci due numeri: \n");
+    scanf("%d %d", &a, &b);
+    
+    if (a > b) 
+    {
+
+    
+
+
+    printf ("maggiore: \n");
+    }
+else if (a == b)
+{
+
+
+printf ("uguale: \n");
+
+}
+
+ else 
+{
+    printf ("minore: \n");
+}
+}
